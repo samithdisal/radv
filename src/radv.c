4216baa42b60b13a29e73eee71d8009ba8b1b4a5
Samith Sandanayake <samithdisal@gmail.com>
2023-04-28 22:20:07 +1000

    Initial commit

---
+++ src/radv.c
@@ -0,0 +1,6 @@
+#include "radv.h"
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
---
+++ src/radv.h
@@ -0,0 +1,18 @@
+#if !defined(__RADV_H__)
+#define __RADV_H__
+
+typedef struct Context
+{
+
+} Context;
+
+
+
+
+#endif // __RADV_H__
+#if !defined(__RADV_H__)
+#define __RADV_H__
+
+
+
+#endif // __RADV_H__
